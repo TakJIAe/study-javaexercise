ffffacc6ead8466b173fe9d0e47170f4dc8cd32c
TakJIAe <hiworldd@naver.com>
2021-06-29 16:00:56 +0900

    연습문제 3-6

---
+++ ch3_연산자/3-6/Exercise3_6.java
@@ -0,0 +1,7 @@
+class Exercise3_6 {
+    public static void main(String[] args) {
+        int num = 24;
+        System.out.println(10-num%10);
+        System.out.println((num/10+1)*10-num);
+    }    
+}
